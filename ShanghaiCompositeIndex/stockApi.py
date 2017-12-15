ecd3a23ec769bc7bdca91e55789e4d0b17d8e138
zcfan <zchaofan08@gmail.com>
2017-12-15 23:17:45 +1100

    Get stock Data

---
+++ ShanghaiCompositeIndex/stockApi.py
@@ -0,0 +1,9 @@
+"""
+@tushare: https://pypi.python.org/pypi/tushare#downloads
+@source: https://github.com/dengdaiyemanren/python/wiki/%E8%8E%B7%E5%8F%96%E4%BA%A4%E6%98%93%E6%95%B0%E6%8D%AEAPI%E6%B1%87%E6%80%BB
+
+"""
+
+import tushare as ts
+
+ts.get_hist_data('600848') #一次性获取全部日k线数据
